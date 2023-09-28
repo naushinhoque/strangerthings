d350ab76591b081116209a953c720634eb69feeb
naushinhoque <fnmh@yahoo.com>
2023-09-28 17:06:52 -0500

    episode done

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -3,11 +3,38 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
 
-test('renders without errors', () => { });
+const testShow = {
+    name: "test show",
+    summary: "test summary",
+    season: [
+        {
+            id:0,
+            name: "Season 1",
+            episode: []
+        },
+        {
+            id: 1,
+            name: "Season 2",
+            episode: []
+        }
+    ]
+}
 
-test('renders Loading component when prop show is null', () => { });
+test('renders without errors', () => {
+    render(<Show show={testShow} seclectedSeason={"none"} />)
+ });
 
-test('renders same number of options seasons are passed in', () => { });
+test('renders Loading component when prop show is null', () => { 
+    render(<Show show={null} />);
+    const loading = screen.queryByTestId('season-option');
+    expect(loading).toBeInTheDocument();
+});
+
+test('renders same number of options seasons are passed in', () => { 
+    render(<Show show={testShow} selectedSeason={"none"} />);
+    const seasonOptions = screen.queryAllByTestId('season-option')
+    expect(seasonOptions).toHaveLength(2);
+});
 
 test('handleSelect is called when an season is selected', () => { });
 
