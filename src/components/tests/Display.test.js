7c58635a600c6a01386508788057aa1a033892a8
naushinhoque <fnmh@yahoo.com>
2023-09-29 12:15:38 -0500

    display

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,11 +1,38 @@
-import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
 
-test('renders without errors with no props', async () => { });
+import mockFetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow')
 
-test('renders Show component when the button is clicked ', () => { });
+const testShow = {
+    name: "test show",
+    summary: "test summary",
+    season: [
+        {
+            id:0,
+            name: "Season 1",
+            episode: []
+        },
+        {
+            id: 1,
+            name: "Season 2",
+            episode: []
+        }
+    ]
+}
+
+test('renders without errors with no props', async () => {
+    render(<Display />)
+ });
+
+test('renders Show component when the button is clicked ', () => {
+    mockFetchShow.mockResolvedValueOnce(testShow);
+    
+    render(<Display />);
+    const button = screen.getByRole('button');
+    userEvent.clickButton();
+ });
 
 test('renders show season options matching your data when the button is clicked', () => { });
 
